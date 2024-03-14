7d9b945c65ed155a03226da737d9c43fabcd14c6
waterwoodming <162718260+waterwoodming@users.noreply.github.com>
2024-03-14 17:37:32 +0800

    Delete KF_environment directory

--- KF_environment/bochs/bochs-2.7/bxversion.h
+++
@@ -1,9 +0,0 @@
-/////////////////////////////////////////////////////////////////////////
-// This file is checked in as bxversion.h.in.  The configure script
-// substitutes variables and creates bxversion.h.
-/////////////////////////////////////////////////////////////////////////
-
-#define VERSION       "2.7"
-#define VER_SVNFLAG   0
-#define REL_STRING    "Built from SVN snapshot on August  1, 2021"
-#define REL_TIMESTAMP "Sun Aug  1 10:07:00 CEST 2021"
